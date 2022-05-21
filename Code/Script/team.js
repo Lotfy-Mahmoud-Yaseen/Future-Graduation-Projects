6382441f0498745cdc3cc782aa92487760191b09
Lotfy-Mahmoud-Yaseen <lotfymahmoudyaseen@gmail.com>
2022-05-21 04:48:52 +0200

    M

--- Code/Script/team.js
+++ Code/Script/team.js
@@ -1,3 +1,7 @@
 function togglepopup() {
     document.getElementById("popup-1").classList.toggle("active");
 }
+
+function git() {
+    document.getElementById("Git-1").classList.toggle("active");
+}
